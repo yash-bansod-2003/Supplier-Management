fb87d40a626275bd1c223325e557d8015b4978ae
yash bansod <yashbansodgit@gmail.com>
2024-03-11 11:43:43 +0530

    fix: extra imports

--- app/(landing)/pricing/page.tsx
+++ app/(landing)/pricing/page.tsx
@@ -1,9 +1,3 @@
-import Link from "next/link";
-
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
-import { Icons } from "@/components/icons";
-import { siteConfig } from "@/config/site";
 import { Pricing } from "@/components/landing/pricing";
 
 export const metadata = {
